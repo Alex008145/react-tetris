3ee04fd484d6c97a4141870cfecd30a9addbeb08
Alexey Stashatov <alex008145@gmail.com>
2024-04-08 20:46:27 +0300

    creating tetrominoes

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -2,20 +2,30 @@ import "./Tetris.css";
 
 import Board from "./Board";
 import GameStats from "./GameStats";
+import Previews from "./Previews";
 
 import { useBoard } from "../hooks/useBoard";
+import { useGameStats } from "../hooks/useGameStats";
+import { usePlayer } from "../hooks/usePlayer";
 
 const Tetris = ({ rows, columns, setGameOver }) => {
-  const [board, setBoard] = useBoard({ rows, columns });
+  const [gameStats, addLinesCleared] = useGameStats({ rows, columns });
+  const [player, setPlayer, resetPlayer] = usePlayer();
+  const [board, setBoard] = useBoard({
+    rows,
+    columns,
+    player,
+    resetPlayer,
+    addLinesCleared,
+  });
 
   return (
     <div className="Tetris">
       <Board board={board} />
-      <GameStats gameStats={GameStats} />
+      <GameStats gameStats={gameStats} />
+      <Previews tetrominoes={player.tetrominoes} />
     </div>
   );
-
-  return;
 };
 
 export default Tetris;
